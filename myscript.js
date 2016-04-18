c149672c34b66ae2ddb21b4daacee1e02a1e1534
mlamont8 <debiandmiat@hotmail.com>
2016-04-18 19:46:15 -0400

    visual updates

--- myscript.js
+++ myscript.js
@@ -5,6 +5,8 @@ var display = document.querySelector('.timer');
 function restTimer() {
     var ttime = $('#reset').text();
     var setMinutes = ttime * 60;
+    $('status').empty();
+    $('status').html('RESETTING');
     startTimer(setMinutes, display);
     resetToggle = 1;
 }
@@ -26,7 +28,7 @@ function startTimer(duration, display) {
         display.textContent = minutes + ":" + seconds;
 
         if (--timer < 0) {
-            if (resetToggle == 0) {
+            if (resetToggle === 0) {
                 timer = null;
                 clearInterval(timer);
                 restTimer();
@@ -41,7 +43,8 @@ function startTimer(duration, display) {
 
 
 $(document).ready(function () {
-
+$("#spinner, .pauseButton").hide();
+   
     var display = document.querySelector('.timer');
     $('button').addClass('disabled');
 
@@ -68,11 +71,14 @@ $(document).ready(function () {
         });
     });
 
-    //Starts Timer on Click
+    //When clicker div is clicked, hourglass is removed, spinner is shown until either another click or timer is expired.
 
-    $(".glass").click(function () {
+    $(".clicker").click(function () {
         var ttime = $('#ttime').text();
         var setMinutes = ttime * 60;
+        $(".mainButton, .adjustButton").hide();
+        $(".pauseButton, #spinner").show();
+        $('.status').html('TIMER');
         startTimer(setMinutes, display);
 
     });
