56e84db2628cc8b2fc436003bca454936182af26
mlamont8 <debiandmiat@hotmail.com>
2016-04-25 16:23:29 -0400

    timer adjustment

--- myscript.js
+++ myscript.js
@@ -1,4 +1,5 @@
 var resetToggle = 0;
+var isPaused = false;
 var display = document.querySelector('.timer');
 
 //Runs Reset after Initial Run
@@ -11,11 +12,12 @@ function restTimer() {
     resetToggle = 1;
 }
 //Runs Timer
-function startTimer(duration, display) {
+function startTimer(duration, display, pause) {
     var timer = duration;
     var minutes = "0";
     var seconds = "0";
     console.log(timer);
+    if(!pause){
     setInterval(function () {
         var display = document.querySelector('.timer');
         console.log(display);
@@ -38,12 +40,15 @@ function startTimer(duration, display) {
 
         }
     }, 1000);
-
+    }
+}
+function pauseTimer (currentMin){
+    
 }
 
 
 $(document).ready(function () {
-$("#spinner, .pauseButton").hide();
+$("#spinner, .pauseButton, .resumeButton").hide();
    
     var display = document.querySelector('.timer');
     $('button').addClass('disabled');
@@ -67,7 +72,7 @@ $("#spinner, .pauseButton").hide();
     });
     $("#glyphMinusReset").click(function () {
         $('#reset').html(function (i, val) {
-            return +val + 1;
+            return +val - 1;
         });
     });
 
@@ -78,8 +83,15 @@ $("#spinner, .pauseButton").hide();
         var setMinutes = ttime * 60;
         $(".mainButton, .adjustButton").hide();
         $(".pauseButton, #spinner").show();
-        $('.status').html('TIMER');
-        startTimer(setMinutes, display);
-
+       // $('.status').html('TIMER');
+        var pause = false;
+        startTimer(setMinutes, display, pause);
+// When Pause button is selected, timer is paused, spinner is removed, resume button is shown, pause button is removed.
     });
+    $(".pauseButton").click(function(){
+        $(".pauseButton, #spinner").hide();
+        $(".resumeButton").show();
+        e.preventDefault();
+    });
+
 });
